870f808960689900dd9ef30b682b72c5853f1ae3
calamitymike <notcalamitymike@gmail.com>
2024-12-11 05:53:11 -0500

    Remove boilerplate code and create a simple mockup of the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [onboardStep, setOnboardStep] = useState(0);
+  const onBoardPages = [
+    <div key='0'>Page 1 - Email & Password</div>,
+    <div key='1' id='page=1 page=2'>
+      Page 2 - Birthday & About Me
+    </div>,
+    <div key='1'>Page 3 - Address</div>,
+    <div key='1'>Page 4 - Terms And Conditions</div>,
+  ];
+
+  function handleNextClick() {
+    setOnboardStep((onboardStep) => onboardStep + 1);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id='appContainer'>
+      {onBoardPages[onboardStep % onBoardPages.length]}
+      <button onClick={handleNextClick}>Next</button>
+    </div>
+  );
 }
 
-export default App
+export default App;

80485535c50b0aab3d8d3fdd25e240270e70d5b1
calamitymike <notcalamitymike@gmail.com>
2024-12-13 00:31:33 -0500

    Build out the Data component by fetching all user data from the express server. Display all user data in a table. Install CORS package for use on the express server to enable cross origin resource sharing

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import pg from 'pg';
-// import fs from 'fs';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3001;
+app.use(cors());
 
 const databaseURL = '';
 
@@ -16,25 +17,13 @@ app.get('/api', (req, res) => {
   res.send('Response from express server');
 });
 
-// let tempdbdata = '';
-
-// fs.readFile('./server/tempdatabase.json', (err, data) => {
-//   if (err) {
-//     console.error('Error reading file:', err);
-//   } else {
-//     console.log('File content (string):', data.toString());
-//     tempdbdata = data.toString();
-//   }
-// });
-
-app.get('/api/allusers', async (req, res) => {
+app.get('/api/data', async (req, res) => {
   try {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM user_data');
     const users = result.rows;
     client.release();
     res.status(200).json(users);
-    // res.send(tempdbdata);
   } catch (error) {
     console.error('Error fetching users:', error);
     res.status(500).json({ error: 'ERROR: Failed to fetch users' });
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,3 +1,71 @@
+import { useState, useEffect } from 'react';
+
 export default function Data() {
-  return <div>Data Component</div>;
+  const [userData, setUserData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      setIsLoading(true);
+      try {
+        const data = await fetch('http://localhost:3001/api/data');
+        const dataJSON = await data.json();
+        setUserData(dataJSON);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getUserData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading Data</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching user data {error.message}</div>;
+  }
+
+  return (
+    <div id='dataContainer'>
+      <div id='userDataContainer'>
+        <h2>User Data</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Email Address</th>
+              <th>Hashed Password</th>
+              <th>About Me</th>
+              <th>Birthday</th>
+              <th>Street Address</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Zip Code</th>
+              <th>Country</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userData.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.email_address}</td>
+                <td>{user.password}</td>
+                <td>{user.about_me}</td>
+                <td>{new Date(user.birthday).toLocaleDateString()}</td>
+                <td>{user.address_street}</td>
+                <td>{user.address_city}</td>
+                <td>{user.address_state}</td>
+                <td>{user.address_zipcode}</td>
+                <td>{user.address_country}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }

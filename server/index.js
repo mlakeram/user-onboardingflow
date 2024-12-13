e6b72b6ad936a6dfdb200c5fc663de6ec889292a
calamitymike <notcalamitymike@gmail.com>
2024-12-12 21:46:56 -0500

    Create a /api/allusers endpoint on the express server that fetches all users in the database and returns it as JSON data

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import pg from 'pg';
+// import fs from 'fs';
 
 const app = express();
 const PORT = 3001;
@@ -15,6 +16,17 @@ app.get('/api', (req, res) => {
   res.send('Response from express server');
 });
 
+// let tempdbdata = '';
+
+// fs.readFile('./server/tempdatabase.json', (err, data) => {
+//   if (err) {
+//     console.error('Error reading file:', err);
+//   } else {
+//     console.log('File content (string):', data.toString());
+//     tempdbdata = data.toString();
+//   }
+// });
+
 app.get('/api/allusers', async (req, res) => {
   try {
     const client = await pool.connect();
@@ -22,6 +34,7 @@ app.get('/api/allusers', async (req, res) => {
     const users = result.rows;
     client.release();
     res.status(200).json(users);
+    // res.send(tempdbdata);
   } catch (error) {
     console.error('Error fetching users:', error);
     res.status(500).json({ error: 'ERROR: Failed to fetch users' });

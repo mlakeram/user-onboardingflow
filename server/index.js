e9ae91a367397a008ba8a8ad5414a88ee0fd2d78
calamitymike <notcalamitymike@gmail.com>
2024-12-15 06:24:40 -0500

    Test

--- server/index.js
+++ server/index.js
@@ -5,11 +5,7 @@ import path from 'path';
 
 const app = express();
 const PORT = 3001;
-app.use(
-  cors({
-    origin: 'https://useronboardingflow-a83jqj0bq-calamitylabs.vercel.app',
-  })
-);
+app.use(cors());
 
 const databaseURL =
   'postgres://emjzlloo:KXquwbCINBQ8ukluBBuID_r8ujrej1F6@lallah.db.elephantsql.com/emjzlloo';
